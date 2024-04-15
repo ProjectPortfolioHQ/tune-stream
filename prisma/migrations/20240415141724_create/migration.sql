e997740cb5d81e3c21d7e9006ca832e7cd11df3c
Cuc Trinh <huucucdev@gmail.com>
2024-04-15 21:22:19 +0700

    setup prisma and create table album

---
+++ prisma/migrations/20240415141724_create/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Album` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `artist` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `status` ENUM('ACTIVE', 'IN_ACTIVE') NOT NULL DEFAULT 'ACTIVE',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
